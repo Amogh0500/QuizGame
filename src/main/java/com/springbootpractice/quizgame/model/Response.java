991d8d1871137e59377fa90e3eb49837e0bf66c0
amoghburte <amoghburte05@gmail.com>
2024-03-21 17:06:48 +0530

    application.properties changes

--- src/main/java/com/springbootpractice/quizgame/model/Response.java
+++ src/main/java/com/springbootpractice/quizgame/model/Response.java
@@ -1,9 +1,14 @@
 package com.springbootpractice.quizgame.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class Response {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String response;
 }
